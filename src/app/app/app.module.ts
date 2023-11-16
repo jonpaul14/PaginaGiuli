c4f1d0c3990983be2286d214a85866c53e2d3364
jonpaul <jonpaul_14@hotmail.com>
2023-11-15 19:09:50 -0500

    firt commit

---
+++ src/app/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {AppsRoutingModule} from "./Apps-routing.module";
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AppsRoutingModule
+  ]
+})
+export class AppModule { }
---
+++ src/app/app/hoja1/hoja1.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-hoja1',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './hoja1.component.html',
+  styleUrl: './hoja1.component.css'
+})
+export class Hoja1Component {
+
+}
